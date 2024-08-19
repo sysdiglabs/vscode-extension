3b9f84dd4a278d0f193d5a73fc8eeaa2d5a7f09a
Fede Barcelona <fede_rico_94@hotmail.com>
2024-08-19 14:24:48 +0200

    build: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          in
+          f pkgs
+        );
+    in
+    {
+      devShells = forEachSystem (
+        pkgs: with pkgs; {
+          default = mkShell {
+            buildInputs = [
+              nodejs_22
+              typescript
+              vsce
+            ];
+          };
+        }
+      );
+
+      formatter = forEachSystem (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
