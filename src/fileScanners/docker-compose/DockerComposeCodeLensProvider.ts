b912128ccaf3b95070747631b8ccf73031d8ceeb
Fede Barcelona <fede_rico_94@hotmail.com>
2024-08-20 15:45:03 +0200

    fix: Change the docker-compose codelens provider to use the ast

--- src/fileScanners/docker-compose/DockerComposeCodeLensProvider.ts
+++ src/fileScanners/docker-compose/DockerComposeCodeLensProvider.ts
@@ -1,62 +1,41 @@
 import * as vscode from 'vscode';
-import * as yaml from 'js-yaml';
-import { grepString } from '../highlighters';
+import * as yaml from '../../utils/yaml';
 import { isComposeFile } from './dockercomposeScanner';
 
 export class DockerComposeCodeLensProvider implements vscode.CodeLensProvider {
-    private onDidChangeCodeLensesEmitter = new vscode.EventEmitter<void>();
+  private onDidChangeCodeLensesEmitter = new vscode.EventEmitter<void>();
 
-    public readonly onDidChangeCodeLenses: vscode.Event<void> = this.onDidChangeCodeLensesEmitter.event;
+  public readonly onDidChangeCodeLenses: vscode.Event<void> = this.onDidChangeCodeLensesEmitter.event;
 
-    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] {
-        const codeLenses: vscode.CodeLens[] = [];
+  public provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.CodeLens[] {
+    const codeLenses: vscode.CodeLens[] = [];
 
-        if (!document || !isComposeFile(document)) {
-            return codeLenses;
-        }
-
-        let scannedImages: string[] = [];
-        
-        let command: vscode.Command = {
-            title: "$(rocket) Scan Docker Compose",
-            command: "sysdig-vscode-ext.scanDockerCompose",
-            arguments: [document],
-            tooltip: "Scan Docker Compose file for image vulnerabilities"
-        };
-        codeLenses.push(new vscode.CodeLens(new vscode.Range(0,0,0,0), command));
-
-        const content: any = yaml.load(document.getText());
-
-
-        if (content.services) {
-            for (const serviceName in content.services) {
-                const service = content.services[serviceName];
-                const image = service.image;                    // What about service.build? Should we handle duplicate instances of the same image to avoid rescan?
-
-                if (!image || scannedImages.includes(image)) {
-                    continue;
-                }
-
-                scannedImages.push(image);
-                let ranges : vscode.Range[] | undefined = grepString(document, image);
-
-                if (ranges) {
-                    for (const range of ranges) {
-                        command = {
-                            title: "$(beaker) Scan Image",
-                            command: "sysdig-vscode-ext.scanImage",
-                            arguments: [image, true, document, range],
-                            tooltip: "Scan image for vulnerabilities"
-                        };
-                        codeLenses.push(new vscode.CodeLens(range, command));
-                    }
-                }
-            }
-        }
-        return codeLenses;
+    if (!document || !isComposeFile(document)) {
+      return codeLenses;
     }
 
-    public refresh() {
-        this.onDidChangeCodeLensesEmitter.fire();
+    let command: vscode.Command = {
+      title: "$(rocket) Scan Docker Compose",
+      command: "sysdig-vscode-ext.scanDockerCompose",
+      arguments: [document],
+      tooltip: "Scan Docker Compose file for image vulnerabilities"
+    };
+    codeLenses.push(new vscode.CodeLens(new vscode.Range(0, 0, 0, 0), command));
+
+    const imagesAndRanges = yaml.extractImagesAndRanges(document);
+    for (const imageWithRange of imagesAndRanges) {
+      command = {
+        title: "$(beaker) Scan Image",
+        command: "sysdig-vscode-ext.scanImage",
+        arguments: [imageWithRange.image, true, document, imageWithRange.range],
+        tooltip: "Scan image for vulnerabilities"
+      };
+      codeLenses.push(new vscode.CodeLens(imageWithRange.range, command));
     }
+    return codeLenses;
+  }
+
+  public refresh() {
+    this.onDidChangeCodeLensesEmitter.fire();
+  }
 }
