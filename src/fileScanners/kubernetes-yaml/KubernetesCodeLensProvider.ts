fa3fe059b7048331ff5d27ef66adcec535eb59bb
Fede Barcelona <fede_rico_94@hotmail.com>
2024-08-20 15:44:54 +0200

    fix: Change the kubernetes codelens provider to use the ast

--- src/fileScanners/kubernetes-yaml/KubernetesCodeLensProvider.ts
+++ src/fileScanners/kubernetes-yaml/KubernetesCodeLensProvider.ts
@@ -1,63 +1,39 @@
 import * as vscode from 'vscode';
-import * as yaml from 'js-yaml';
-import { grepString } from '../highlighters';
+import * as yaml from '../../utils/yaml';
 import { isKubernetesFile } from './kubernetesScanner';
+
 
 export class KubernetesCodeLensProvider implements vscode.CodeLensProvider {
     private onDidChangeCodeLensesEmitter = new vscode.EventEmitter<void>();
 
     public readonly onDidChangeCodeLenses: vscode.Event<void> = this.onDidChangeCodeLensesEmitter.event;
 
-    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] {
+    public provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.CodeLens[] {
         const codeLenses: vscode.CodeLens[] = [];
 
         if (!document || !isKubernetesFile(document)) {
             return codeLenses;
         }
 
-        let scannedImages: string[] = [];
-        
         let command: vscode.Command = {
             title: "$(rocket) Scan manifest",
             command: "sysdig-vscode-ext.scanKubernetes",
             arguments: [document],
             tooltip: "Scan Kubernetes manifest for image vulnerabilities (in Pods or Deployments)"
         };
-        codeLenses.push(new vscode.CodeLens(new vscode.Range(0,0,0,0), command));
+        codeLenses.push(new vscode.CodeLens(new vscode.Range(0, 0, 0, 0), command));
 
-        const content: any = yaml.load(document.getText());
-        let containers : any;
-        if (content.kind === 'Deployment') {
-            containers = content?.spec?.template?.spec?.containers || [];
-        } else if (content.kind === 'Pod') {
-            containers = content?.spec?.containers || [];
-        } else {
-            return codeLenses;
+        let imagesAndRanges = yaml.extractImagesAndRanges(document);
+        for (const imageWithRange of imagesAndRanges) {
+            command = {
+                title: "$(beaker) Scan Image",
+                command: "sysdig-vscode-ext.scanImage",
+                arguments: [imageWithRange.image, true, document, imageWithRange.range],
+                tooltip: "Scan image for vulnerabilities"
+            };
+            codeLenses.push(new vscode.CodeLens(imageWithRange.range, command));
         }
 
-        for (const container of containers) {
-            const image = container.image;
-
-            if (!image || scannedImages.includes(image)) {
-                continue;
-            }
-
-            scannedImages.push(image);
-            let ranges : vscode.Range[] | undefined = grepString(document, image);
-
-            if (ranges) {
-                for (const range of ranges) {
-                    command = {
-                        title: "$(beaker) Scan Image",
-                        command: "sysdig-vscode-ext.scanImage",
-                        arguments: [image, true, document, range],
-                        tooltip: "Scan image for vulnerabilities"
-                    };
-                    codeLenses.push(new vscode.CodeLens(range, command));
-                }
-            }
-        }
-        
         return codeLenses;
     }
 
