0ee3e016225a9a48831bda2ae70c82ed2feeaec4
Fede Barcelona <fede_rico_94@hotmail.com>
2024-08-24 00:44:35 +0200

    build(nix): build extension with nix and add "code" app
    Building the extension with nix will ensure the reproducibility.
    Also the new app "code" that can be executed with `nix run .#code`
    allows to launch a VSCode instance with the extension installed.
    This can be used for testing or demo purposes.

--- flake.nix
+++ flake.nix
@@ -26,16 +26,49 @@
         );
     in
     {
+      packages = forEachSystem (
+        pkgs: with pkgs; rec {
+          default = vsix;
+          vsix = callPackage ./vsix.nix { };
+
+          sysdig-vscode-ext = vscode-utils.buildVscodeExtension {
+            inherit (vsix.packageJson) name version;
+            src = vsix;
+            unpackPhase = "unzip $src";
+
+            vscodeExtPublisher = vsix.packageJson.publisher;
+            vscodeExtName = vsix.packageJson.name;
+            vscodeExtUniqueId = "${vsix.packageJson.publisher}.${vsix.packageJson.name}";
+          };
+        }
+      );
+
+      apps = forEachSystem (pkgs: {
+        # Builds the extension and packages is with vscode to launch it.
+        # To execute with: nix run .#code
+        # You can also execute it with the latest version in the repo: nix run github:sysdiglabs/vscode-extension#code
+        # Or even from a tag: nix run github:sysdiglabs/vscode-extension/0.2.6#code
+        code =
+          let
+            vscode-with-extension-installed = pkgs.vscode-with-extensions.override {
+              vscodeExtensions = [ self.packages.${pkgs.system}.sysdig-vscode-ext ];
+            };
+          in
+          {
+            type = "app";
+            program = "${vscode-with-extension-installed}/bin/code";
+          };
+      });
+
       devShells = forEachSystem (
         pkgs: with pkgs; {
           default = mkShell {
             shellHook = ''
-              npm install
+              npm ci
             '';
-
             buildInputs = [
               vscode
-              nodejs_22
+              nodejs
               typescript
               vsce
               nodePackages.typescript-language-server
