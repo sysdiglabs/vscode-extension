fdb4206887870d2595f6aa7f06b96e8d341dd6b9
Fede Barcelona <fede_rico_94@hotmail.com>
2026-06-16 12:29:18 +0200

    chore: pin GitHub Actions to commit SHAs and add pinact
    Pinning actions to immutable commit SHAs prevents supply-chain attacks via
    mutable tags. Add pinact to the dev shell and update task to keep pins fresh.

--- Justfile
+++ Justfile
@@ -20,6 +20,7 @@ update:
     nix develop --command pre-commit autoupdate
     nix develop --command npm update
     nix develop --command just rehash-npm-nix
+    nix develop --command pinact run -u --diff
 
 rehash-npm-nix:
     sd 'npmDepsHash = ".*";' "npmDepsHash = \"$(nix hash convert --to sri $(prefetch-npm-deps ./package-lock.json))\";" vsix.nix
--- flake.nix
+++ flake.nix
@@ -72,15 +72,16 @@
                 pre-commit install
               '';
               packages = [
-                vscode
-                nodejs
-                typescript
-                vsce
-                pre-commit
                 just
-                typescript-language-server
+                nodejs
+                pinact
+                pre-commit
                 prefetch-npm-deps
                 sd
+                typescript
+                typescript-language-server
+                vsce
+                vscode
               ]
               ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
                 xvfb-run
